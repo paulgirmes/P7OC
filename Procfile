ec11f93615610836d2303e1276cb94f30696a6ea
Paul <paulgirmes@hotmail.com>
2020-04-19 12:14:29 +0200

    procfile change

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn app:app
-init: FLASK_APP=run.py
+init: FLASK_APP=run.py flask
--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from app import app
-import os
-import sys
 
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
